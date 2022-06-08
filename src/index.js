3867dc230eee5d5631bae2683b939fbf08f7fba1
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-08 17:34:48 +0300

    passing props

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
-  <div className="ui container comments">
-    <CommentDetail author="Sam"/>
-    <CommentDetail author="Alex"/>
-    <CommentDetail author="Jane"/>
-  </div>
-    );
+    <div className="ui container comments">
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" avatar={faker.image.image()}/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="I like the subject" avatar={faker.image.image()}/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing" avatar={faker.image.image()}/>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
